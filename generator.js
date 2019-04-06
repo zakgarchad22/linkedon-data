37733278b8f0e4d559140dfbacaf79df1947ab4b
jona <jona@elevation.ac>
2019-04-06 10:11:55 +0300

    create script to insert docs for mongo exercises

---
+++ generator.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+const fs = require("fs")
+const data = JSON.parse(fs.readFileSync("people.json"))
+
+mongoose.connect('mongodb://localhost/test-db', { useNewUrlParser: true })
+
+const Person = mongoose.model('Person', new mongoose.Schema({
+    firstName: String,
+    lastName: String,
+    currentCompany: {
+        industry: String,
+        name: String
+    },
+    currentPosition: String,
+    previousCompanies: [
+        {
+            industry: String,
+            name: String
+        }
+    ],
+    salary: Number
+}, { collection: "linkedon" }))
+
+const onInsert = function (err, docs) {
+    if (err) { console.log(err) }
+    else { console.info('Done'); mongoose.disconnect() }
+}
+
+Person.collection.insert(data, onInsert)
